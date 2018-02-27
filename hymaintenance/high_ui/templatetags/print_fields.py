e16a3c87f436461c1ca2c6a987aadb4056b26d12
Niouby <morgane.alonso@protonmail.com>
2018-02-27 13:35:32 +0100

    modify template, print correct time according to counter type

--- hymaintenance/high_ui/templatetags/print_fields.py
+++ hymaintenance/high_ui/templatetags/print_fields.py
@@ -1,5 +1,6 @@
 
 from django.template import Library
+from django.utils.html import mark_safe
 
 
 register = Library()
@@ -24,3 +25,14 @@ def pretty_print_minutes(value, use_long_minute_format=False):
             return "%s%s mins" % (negative_op, minutes)
         return "%s%sm" % (negative_op, minutes)
     return "%s%sh" % (negative_op, value // 60)
+
+
+@register.simple_tag
+def pretty_print_contract_counter(contract):
+    counter = ""
+    if contract.total_type == 0:
+        counter = pretty_print_minutes(contract.get_number_remaining_minutes())
+        counter += " /&nbsp;" + str(contract.get_number_contract_hours()) + "h"
+    elif contract.total_type == 1:
+        counter = pretty_print_minutes(contract.get_number_consumed_minutes())
+    return mark_safe(counter)
