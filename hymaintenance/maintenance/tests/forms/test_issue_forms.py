c47796a7a3d4ddd7a0d12da945e28149fc6fda2d
Niouby <morgane.alonso@protonmail.com>
2018-03-14 13:49:28 +0100

    Refs #31 add test for maintenance issue update form

--- hymaintenance/maintenance/tests/forms/test_issue_forms.py
+++ hymaintenance/maintenance/tests/forms/test_issue_forms.py
@@ -2,9 +2,18 @@ from django.test import TestCase
 from django.utils.timezone import now
 
 from customers.tests.factories import CompanyFactory, MaintenanceUserFactory
-from maintenance.tests.factories import IncomingChannelFactory, MaintenanceConsumerFactory, MaintenanceTypeFactory
+from maintenance.tests.factories import IncomingChannelFactory, MaintenanceConsumerFactory, MaintenanceIssueFactory, MaintenanceTypeFactory
 
-from ...forms import MaintenanceIssueCreateForm
+from ...forms import MaintenanceIssueCreateForm, MaintenanceIssueUpdateForm, duration_in_minutes
+
+
+class DurationFunctionTestCase(TestCase):
+
+    def test_when_i_pass_hours_duration_time(self):
+        self.assertEqual(60, duration_in_minutes(1, "hours"))
+
+    def test_when_i_pass_minutes_duration_time(self):
+        self.assertEqual(60, duration_in_minutes(60, "minutes"))
 
 
 class IssueCreateFormTestCase(TestCase):
@@ -30,7 +39,7 @@ class IssueCreateFormTestCase(TestCase):
                 "duration_type": "hours",
                 "duration": 2}
 
-    def test_when_i_bound_a_form_with_invalid_duration_type_i_have_an_error(self):
+    def test_when_i_bound_a_create_form_with_invalid_duration_type_i_have_an_error(self):
 
         subject = "subject of the issue"
         description = "Description of the Issue"
@@ -44,7 +53,7 @@ class IssueCreateFormTestCase(TestCase):
         self.assertEqual(1, len(form.errors))
         self.assertEqual(form.errors['duration'].as_text(), "* Invalid duration type: 'years'")
 
-    def test_when_i_bound_a_form_with_invalid_duration_i_have_an_error(self):
+    def test_when_i_bound_a_create_form_with_invalid_duration_i_have_an_error(self):
 
         subject = "subject of the issue"
         description = "Description of the Issue"
@@ -54,6 +63,59 @@ class IssueCreateFormTestCase(TestCase):
         dict_for_post["duration"] = "0"
 
         form = MaintenanceIssueCreateForm(company=self.company, data=dict_for_post)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(1, len(form.errors))
+        self.assertEqual(form.errors['duration'].as_text(), "* Invalid duration: '0'")
+
+
+class IssueUpdateFormTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = MaintenanceUserFactory(email="gordon.freeman@blackmesa.com",
+                                          password="azerty")
+
+        cls.company = CompanyFactory()
+        cls.maintenance_type = MaintenanceTypeFactory()
+        cls.channel = IncomingChannelFactory()
+        cls.consumer = MaintenanceConsumerFactory(company=cls.company)
+        cls.issue = MaintenanceIssueFactory(company=cls.company)
+
+    def __get_dict_for_post(self, subject, description):
+        return {"consumer_who_ask": self.consumer.pk,
+                "user_who_fix": self.user.pk,
+                "incoming_channel": self.channel.pk,
+                "subject": subject,
+                "date": now().date(),
+                "maintenance_type": self.maintenance_type.pk,
+                "description": description,
+                "duration_type": "hours",
+                "duration": 2}
+
+    def test_when_i_bound_a_update_form_with_invalid_duration_type_i_have_an_error(self):
+
+        subject = "subject of the issue"
+        description = "Description of the Issue"
+
+        dict_for_post = self.__get_dict_for_post(subject, description)
+
+        dict_for_post["duration_type"] = "years"
+
+        form = MaintenanceIssueUpdateForm(instance=self.issue, data=dict_for_post)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(1, len(form.errors))
+        self.assertEqual(form.errors['duration'].as_text(), "* Invalid duration type: 'years'")
+
+    def test_when_i_bound_a_update_form_with_invalid_duration_i_have_an_error(self):
+
+        subject = "subject of the issue"
+        description = "Description of the Issue"
+
+        dict_for_post = self.__get_dict_for_post(subject, description)
+
+        dict_for_post["duration"] = "0"
+
+        form = MaintenanceIssueUpdateForm(instance=self.issue, data=dict_for_post)
         self.assertFalse(form.is_valid())
         self.assertEqual(1, len(form.errors))
         self.assertEqual(form.errors['duration'].as_text(), "* Invalid duration: '0'")
