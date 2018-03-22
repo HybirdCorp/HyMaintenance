42f8ff9485706ada60521aa45d18c6c4c0e72176
Niouby <morgane.alonso@protonmail.com>
2018-03-22 14:43:06 +0100

    remove and replace label_for_company_detailview with name

--- hymaintenance/maintenance/tests/models/test_other_models.py
+++ hymaintenance/maintenance/tests/models/test_other_models.py
@@ -9,8 +9,7 @@ class MaintenanceTypeTestCase(TestCase):
     def test_i_can_create_a_maintenance_type(self):
         MaintenanceType.objects.all().delete()
         MaintenanceType.objects.create(name="Support",
-                                       css_class="type-support",
-                                       label_for_company_detailview="Support Label")
+                                       css_class="type-support")
         self.assertEqual(1, MaintenanceType.objects.count())
 
     def test_str_is_good_for_maintenance_typ(self):
