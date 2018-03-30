4fb468acf0194e736767140778f4f4ce76e8932d
Niouby <morgane.alonso@protonmail.com>
2018-03-30 14:02:31 +0200

    display custom counter name when it exists

--- hymaintenance/maintenance/models/contract.py
+++ hymaintenance/maintenance/models/contract.py
@@ -18,8 +18,8 @@ class MaintenanceContract(models.Model):
     TYPE_CHOICES = ((AVAILABLE_TOTAL_TIME, _("Available total time")), (CONSUMMED_TOTAL_TIME, _("Consummed total time")))
 
     counter_name = models.CharField(_("Name of counter"), max_length=255, default="")
-    company = models.ForeignKey(Company, verbose_name=_("Company"), on_delete=models.PROTECT)
-    maintenance_type = models.ForeignKey(MaintenanceType, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, verbose_name=_("Company"), on_delete=models.PROTECT, related_name='contracts')
+    maintenance_type = models.ForeignKey(MaintenanceType, on_delete=models.PROTECT, related_name='contracts')
     visible = models.BooleanField(_("Visible to customer user"), default=True)
     start = models.DateField(_("Start Date"), default=datetime.date.today)
     number_hours = models.PositiveIntegerField("Number of Hours by contract", default=0)
@@ -28,6 +28,9 @@ class MaintenanceContract(models.Model):
 
     def __str__(self):
         return '%s , %s' % (self.company, self.maintenance_type)
+
+    def get_counter_name(self):
+        return self.counter_name if self.counter_name != "" else self.maintenance_type.name
 
     def get_number_contract_hours(self) -> int:
         hours_sum = MaintenanceCredit.objects.filter(company=self.company,
