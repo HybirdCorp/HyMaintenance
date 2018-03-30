1400107544143284fe16eb218d3744ab9360ed30
Hugo Smett <hsmett@hybird.org>
2018-03-30 15:18:47 +0200

    remove defaults='' where not blank

--- hymaintenance/customers/models/company.py
+++ hymaintenance/customers/models/company.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.urls import reverse
 
@@ -6,7 +5,8 @@ from django.urls import reverse
 # TODO: clean up the semantics of the 2 "names"
 class Company(models.Model):
     name = models.CharField("name", max_length=255)
-    maintenance_contact = models.CharField("name of internal contact", default="", max_length=500)
+    maintenance_contact = models.CharField("name of internal contact",
+                                           max_length=500)
 
     def __str__(self):
         return self.name
