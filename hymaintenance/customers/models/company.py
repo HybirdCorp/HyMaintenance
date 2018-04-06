af34b873a7eae61e4007902ca641190fc7ca17e0
Niouby <morgane.alonso@protonmail.com>
2018-04-06 16:19:22 +0200

    Refs #1 add company_issue_number in issue and issues_counter in company

--- hymaintenance/customers/models/company.py
+++ hymaintenance/customers/models/company.py
@@ -7,6 +7,7 @@ class Company(models.Model):
     name = models.CharField("name", max_length=255)
     maintenance_contact = models.CharField("name of internal contact",
                                            max_length=500)
+    issues_counter = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
