aea17f04ba68b2af57b2201128ac429634e03e79
Niouby <morgane.alonso@protonmail.com>
2018-04-09 16:59:17 +0200

    remove todo comment on company model

--- hymaintenance/customers/models/company.py
+++ hymaintenance/customers/models/company.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.urls import reverse
 
 
-# TODO: clean up the semantics of the 2 "names"
 class Company(models.Model):
     name = models.CharField("name", max_length=255)
     maintenance_contact = models.CharField("name of internal contact",
                                            max_length=500)
     issues_counter = models.PositiveIntegerField(default=0)
-    # TODO mettre deferred le champ issues_counter
 
     def __str__(self):
         return self.name
