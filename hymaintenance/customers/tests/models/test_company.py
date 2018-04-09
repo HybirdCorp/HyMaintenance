81f6b9c1a0345db4671e1b457b624d17a8c8b343
Niouby <morgane.alonso@protonmail.com>
2018-04-09 16:58:19 +0200

    Refs #1 add tests on company save function

--- hymaintenance/customers/tests/models/test_company.py
+++ hymaintenance/customers/tests/models/test_company.py
@@ -10,3 +10,13 @@ class CompanyTestCase(TestCase):
         Company.objects.create(name="Hybird",
                                maintenance_contact="One Men in Company")
         self.assertEqual(1, Company.objects.count())
+
+    def test_i_cant_save_again_issue_number(self):
+        company = Company.objects.create(name="Hybird",
+                                         maintenance_contact="One Men in Company")
+        company.name = "Black Mesa"
+        company.issues_counter += 10
+        company.save()
+        company = Company.objects.get(pk=company.pk)
+        self.assertEqual(0, company.issues_counter)
+        self.assertEqual("Black Mesa", company.name)
