391aafddecab5d50824f3d6eecdaab92124fff3e
Niouby <morgane.alonso@protonmail.com>
2018-04-16 18:08:56 +0200

    Refs #32 add attachments in create and update issue form

--- hymaintenance/maintenance/models/issue.py
+++ hymaintenance/maintenance/models/issue.py
@@ -7,6 +7,10 @@ from .consumer import MaintenanceConsumer
 from .other_models import IncomingChannel, MaintenanceType
 
 
+def _get_file_path(instance, filename):
+    return '/'.join(['uploads', instance.company.slug_name, "issue-" + str(instance.company_issue_number), filename])
+
+
 class MaintenanceIssue(models.Model):
     company_issue_number = models.PositiveIntegerField(verbose_name=_("Issue number"))
     company = models.ForeignKey(Company, verbose_name=_("Company"), on_delete=models.PROTECT)
@@ -27,10 +31,13 @@ class MaintenanceIssue(models.Model):
     resolution_date = models.DateTimeField(null=True, blank=True)
     shipping_date = models.DateTimeField(null=True, blank=True)
     answer = models.TextField(null=True, blank=True)
+    context_description_file = models.FileField(null=True, upload_to=_get_file_path)
+    resolution_description_file = models.FileField(null=True, upload_to=_get_file_path)
 
     fields_for_form = ('consumer_who_ask', 'user_who_fix', 'incoming_channel',
                        'subject', 'date', 'maintenance_type', 'description',
-                       'resolution_date', 'shipping_date', 'answer')
+                       'resolution_date', 'shipping_date', 'answer', 'context_description_file',
+                       'resolution_description_file')
 
     class Meta:
         verbose_name = "Issue"
@@ -59,4 +66,10 @@ class MaintenanceIssue(models.Model):
         if self.id is None:
             Company.objects.filter(id=self.company_id).update(issues_counter=models.F('issues_counter') + 1)
             self.company_issue_number = Company.objects.filter(id=self.company_id).values_list('issues_counter', flat=True).first()
+        else:
+            this = MaintenanceIssue.objects.get(id=self.id)
+            if this.context_description_file is not None and this.context_description_file != self.context_description_file:
+                this.context_description_file.delete(save=False)
+            if this.resolution_description_file is not None and this.resolution_description_file != self.resolution_description_file:
+                this.resolution_description_file.delete(save=False)
         super().save(*args, **kwargs)
