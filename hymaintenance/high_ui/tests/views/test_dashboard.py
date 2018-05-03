8b0f02ef095b577f9e713ece7f627c804f92f59e
Niouby <morgane.alonso@protonmail.com>
2018-05-03 14:07:52 +0200

    Refs 131, merge test dashboard files

--- hymaintenance/high_ui/tests/views/test_dashboard.py
+++ hymaintenance/high_ui/tests/views/test_dashboard.py
@@ -2,11 +2,14 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from customers.tests.factories import CompanyFactory
+from customers.tests.factories import CompanyFactory, OperatorUserFactory
 from maintenance.tests.factories import MaintenanceUserFactory
 
 
 class DashboardTestCase(TestCase):
+
+    def setUp(self):
+        self.company = CompanyFactory()
 
     def test_operator_can_seen_the_dashboard(self):
         MaintenanceUserFactory(email="gordon.freeman@blackmesa.com", password="azerty")
@@ -16,9 +19,23 @@ class DashboardTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_manager_cannot_seen_the_dashboard(self):
-        company = CompanyFactory(name="Black Mesa")
-        MaintenanceUserFactory(email="gordon.freeman@blackmesa.com", password="azerty", company=company)
+        MaintenanceUserFactory(email="gordon.freeman@blackmesa.com", password="azerty", company=self.company)
         self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
         response = self.client.get(reverse('high_ui:home'))
 
-        self.assertRedirects(response, company.get_absolute_url())
+        self.assertRedirects(response, self.company.get_absolute_url())
+
+    def test_customer_user_can_seen_this_company(self):
+        OperatorUserFactory(email="other.man@blackmesa.com", password="azerty", first_name="Op1",
+                            last_name="Op1")
+        op2 = OperatorUserFactory(email="gordon.freeman@blackmesa.com", password="azerty", first_name="Op2",
+                                  last_name="Op2")
+        op2.operator_for.add(self.company)
+
+        self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
+        response = self.client.get(reverse('high_ui:home'))
+
+        self.assertEqual(1, self.company.managed_by.count())
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Op2 Op2")
+        self.assertNotContains(response, "Op1 Op1")
