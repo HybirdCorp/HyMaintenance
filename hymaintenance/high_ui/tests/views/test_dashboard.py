bb642ecf518c87bba549edf82854dfc974d0f77f
Niouby <morgane.alonso@protonmail.com>
2018-05-07 10:55:32 +0200

    Refs 131, requested changes done

--- hymaintenance/high_ui/tests/views/test_dashboard.py
+++ hymaintenance/high_ui/tests/views/test_dashboard.py
@@ -25,7 +25,7 @@ class DashboardTestCase(TestCase):
 
         self.assertRedirects(response, self.company.get_absolute_url())
 
-    def test_customer_user_can_seen_this_company(self):
+    def test_operator_user_can_seen_this_company(self):
         OperatorUserFactory(email="other.man@blackmesa.com", password="azerty", first_name="Op1",
                             last_name="Op1")
         op2 = OperatorUserFactory(email="gordon.freeman@blackmesa.com", password="azerty", first_name="Op2",
