3489f10fee144690b0287142a5c95c14f8d264ce
Niouby <morgane.alonso@protonmail.com>
2018-05-14 17:21:24 +0200

    Refs #90, add operators' archive and unarchive forms

--- hymaintenance/customers/tests/forms/test_user_forms.py
+++ hymaintenance/customers/tests/forms/test_user_forms.py
@@ -2,9 +2,9 @@
 from django.test import TestCase
 from django.utils.translation import ugettext_lazy as _
 
-from ...forms import MaintenanceUserCreateForm, ManagerUserCreateForm, OperatorUserCreateForm
+from ...forms import MaintenanceUserCreateForm, ManagerUserCreateForm, OperatorUserArchiveForm, OperatorUserCreateForm, OperatorUserUnarchiveForm
 from ...models import MaintenanceUser
-from ..factories import CompanyFactory
+from ..factories import CompanyFactory, OperatorUserFactory
 
 
 class UserCreateFormTestCase(TestCase):
@@ -58,3 +58,44 @@ class UserCreateFormTestCase(TestCase):
         self.assertTrue(form.is_valid())
         self.assertTrue(form.save(False))
         self.assertEqual(0, MaintenanceUser.objects.filter(is_superuser=False).count())
+
+
+class OperatorUserArchiveFormTestCase(TestCase):
+
+    def setUp(self):
+        self.op1 = OperatorUserFactory(email="chell@aperture-science.com",
+                                       password="azerty",
+                                       is_active=False,
+                                       first_name="Chell")
+        self.op2 = OperatorUserFactory(email="gordon.freeman@blackmesa.com",
+                                       password="azerty",
+                                       first_name="Gordon",
+                                       last_name="Freeman")
+
+    def test_archive_form_update_new_status(self):
+        form = OperatorUserArchiveForm(data={"operators_choices": [self.op2]})
+        self.assertTrue(form.is_valid(), form.errors)
+        form.save()
+        self.assertFalse(MaintenanceUser.objects.get(email="chell@aperture-science.com").is_active)
+        self.assertFalse(MaintenanceUser.objects.get(email="gordon.freeman@blackmesa.com").is_active)
+
+    def test_archive_form_dont_update_when_no_new_status(self):
+        form = OperatorUserArchiveForm(data={"operators_choices": []})
+        self.assertTrue(form.is_valid(), form.errors)
+        form.save()
+        self.assertFalse(MaintenanceUser.objects.get(email="chell@aperture-science.com").is_active)
+        self.assertTrue(MaintenanceUser.objects.get(email="gordon.freeman@blackmesa.com").is_active)
+
+    def test_unarchive_form_update_new_status(self):
+        form = OperatorUserUnarchiveForm(data={"operators_choices": [self.op1]})
+        self.assertTrue(form.is_valid(), form.errors)
+        form.save()
+        self.assertTrue(MaintenanceUser.objects.get(email="chell@aperture-science.com").is_active)
+        self.assertTrue(MaintenanceUser.objects.get(email="gordon.freeman@blackmesa.com").is_active)
+
+    def test_unarchive_form_dont_update_when_no_new_status(self):
+        form = OperatorUserUnarchiveForm(data={"operators_choices": []})
+        self.assertTrue(form.is_valid(), form.errors)
+        form.save()
+        self.assertFalse(MaintenanceUser.objects.get(email="chell@aperture-science.com").is_active)
+        self.assertTrue(MaintenanceUser.objects.get(email="gordon.freeman@blackmesa.com").is_active)
