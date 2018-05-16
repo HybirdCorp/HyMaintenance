6e2a3dd16bb1dd93e724ef6cdfd5820f444b192f
Niouby <morgane.alonso@protonmail.com>
2018-05-16 16:00:58 +0200

    Refs 94, add consumers list update form

--- hymaintenance/maintenance/forms/consumer.py
+++ hymaintenance/maintenance/forms/consumer.py
@@ -10,8 +10,32 @@ class MaintenanceConsumerCreateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.company = kwargs.pop('company')
-        super(MaintenanceConsumerCreateForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def save(self, commit=True):
         self.instance.company = self.company
         return super(MaintenanceConsumerCreateForm, self).save(commit)
+
+
+class MaintenanceConsumersUpdateForm(forms.Form):
+    consumers = forms.ModelMultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        queryset=None
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.company = kwargs.pop('company')
+        super().__init__(*args, **kwargs)
+        self.fields['consumers'].queryset = MaintenanceConsumer.objects.filter(company=self.company)
+        self.fields['consumers'].initial = MaintenanceConsumer.objects.filter(company=self.company, is_used=True)
+
+    def save(self):
+        for consumer in self.cleaned_data['consumers'].filter(is_used=False):
+            consumer.is_used = True
+            consumer.save()
+        consumers_set = set(self.cleaned_data['consumers'])
+        for consumer in self.fields['consumers'].queryset.filter(is_used=True):
+            if consumer not in consumers_set:
+                consumer.is_used = False
+                consumer.save()
