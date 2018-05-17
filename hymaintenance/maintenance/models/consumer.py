5b1c92814195609c9a97b87dedc48b9f65626f5b
Niouby <morgane.alonso@protonmail.com>
2018-05-17 16:05:09 +0200

    Refs 94, display only used consumers on the dashboard

--- hymaintenance/maintenance/models/consumer.py
+++ hymaintenance/maintenance/models/consumer.py
@@ -1,14 +1,22 @@
 
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from customers.models import Company
 
 
+class MaintenanceConsumerManager(BaseUserManager):
+    def get_used_consumers(self):
+        return self.get_queryset().filter(is_used=True).order_by("name")
+
+
 class MaintenanceConsumer(models.Model):
     name = models.CharField(_("Consumer's Name"), max_length=255)
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
     is_used = models.BooleanField(default=True)
+
+    objects = MaintenanceConsumerManager()
 
     def __str__(self):
         return self.name
