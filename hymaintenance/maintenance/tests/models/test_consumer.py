e1d388149318326aef9f2a712290d709ce417d24
Niouby <morgane.alonso@protonmail.com>
2018-05-17 16:05:09 +0200

    Refs 94, display only used consumers on the dashboard

--- hymaintenance/maintenance/tests/models/test_consumer.py
+++ hymaintenance/maintenance/tests/models/test_consumer.py
@@ -17,3 +17,17 @@ class MaintenanceConsumerTestCase(TestCase):
     def test_str_is_good_for_maintenance_consumer(self):
         MaintenanceConsumerFactory(name="Scrooge McDuck")
         self.assertEqual("Scrooge McDuck", str(MaintenanceConsumer.objects.first()))
+
+
+class MaintenanceConsumerManagerTestCase(TestCase):
+    def test_get_used_consumers(self):
+        company = CompanyFactory()
+        MaintenanceConsumer.objects.create(name="Isaac Kleiner",
+                                           company=company,
+                                           is_used=True)
+        MaintenanceConsumer.objects.create(name="Isaac Kleiner",
+                                           company=company,
+                                           is_used=False)
+        self.assertEqual(1, MaintenanceConsumer.objects.get_used_consumers().count())
+        self.assertEqual("Isaac Kleiner",
+                         MaintenanceConsumer.objects.get_used_consumers().first().name)
