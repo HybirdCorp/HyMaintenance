c63fb6d1c78c84e13068f9f69f2c16f742475b14
Morgane Alonso <morgane.alonso@etu.univ-amu.fr>
2018-06-07 17:10:33 +0200

    Refs 168, modify dashboard tests

--- hymaintenance/high_ui/tests/views/test_dashboard.py
+++ hymaintenance/high_ui/tests/views/test_dashboard.py
@@ -6,34 +6,47 @@ from customers.tests.factories import CompanyFactory, ManagerUserFactory, Operat
 
 
 class DashboardTestCase(TestCase):
-
-    def setUp(self):
-        self.company = CompanyFactory()
+    @classmethod
+    def setUpTestData(cls):
+        cls.company = CompanyFactory()
+        cls.page_url = reverse("high_ui:dashboard")
 
     def test_operator_can_seen_the_dashboard(self):
-        user = OperatorUserFactory(email="gordon.freeman@blackmesa.com", password="azerty")
+        user = OperatorUserFactory(
+            email="gordon.freeman@blackmesa.com",
+            password="azerty")
         user.operator_for.add(self.company)
-        self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
-        response = self.client.get(reverse('high_ui:dashboard'))
+
+        self.client.login(username="gordon.freeman@blackmesa.com",
+                          password="azerty")
+        response = self.client.get(self.page_url)
 
         self.assertEqual(response.status_code, 200)
 
     def test_manager_cannot_seen_the_dashboard(self):
-        ManagerUserFactory(email="gordon.freeman@blackmesa.com", password="azerty", company=self.company)
-        self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
-        response = self.client.get(reverse('high_ui:dashboard'))
+        ManagerUserFactory(email="gordon.freeman@blackmesa.com",
+                           password="azerty",
+                           company=self.company)
+
+        self.client.login(username="gordon.freeman@blackmesa.com",
+                          password="azerty")
+        response = self.client.get(self.page_url)
 
         self.assertRedirects(response, self.company.get_absolute_url())
 
     def test_operator_user_can_seen_this_company(self):
-        OperatorUserFactory(email="other.man@blackmesa.com", password="azerty", first_name="Op1",
+        OperatorUserFactory(email="other.man@blackmesa.com",
+                            password="azerty",
+                            first_name="Op1",
                             last_name="Op1")
-        op2 = OperatorUserFactory(email="gordon.freeman@blackmesa.com", password="azerty", first_name="Op2",
+        op2 = OperatorUserFactory(email="gordon.freeman@blackmesa.com",
+                                  password="azerty", first_name="Op2",
                                   last_name="Op2")
         op2.operator_for.add(self.company)
 
-        self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
-        response = self.client.get(reverse('high_ui:dashboard'))
+        self.client.login(username="gordon.freeman@blackmesa.com",
+                          password="azerty")
+        response = self.client.get(self.page_url)
 
         self.assertEqual(1, self.company.managed_by.count())
         self.assertEqual(response.status_code, 200)
