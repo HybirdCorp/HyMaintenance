89140711ef2b0b07d5d1e81a69e0b559fa86670a
Niouby <morgane.alonso@protonmail.com>
2018-07-04 15:43:32 +0200

    Refs 165, add tests on update account view

---
+++ hymaintenance/high_ui/tests/views/test_account_view.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from customers.models import MaintenanceUser
+from customers.tests.factories import AdminUserFactory
+from customers.tests.factories import CompanyFactory
+
+
+class UpdateAccountTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = AdminUserFactory(email="gordon.freeman@blackmesa.com", password="azerty")
+
+        cls.company = CompanyFactory()
+        cls.form_url = reverse("high_ui:update_user")
+
+    def test_manager_can_get_update_form(self):
+        self.client.login(username=self.admin.email, password="azerty")
+        response = self.client.get(self.form_url, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_profile_form(self):
+        first_name = "Barney"
+        last_name = "Calhoun"
+        email = "barney.calhoun@blackmesa.com"
+
+        self.client.login(username=self.admin.email, password="azerty")
+        response = self.client.post(
+            self.form_url,
+            {
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                "confirm_password": "azerty",
+                "form-mod": "profile",
+            },
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Les modifications ont bien été prises en compte!")
+        self.assertEqual(self.admin.pk, MaintenanceUser.objects.get(email=email).pk)
+
+    def test_update_password_form(self):
+        password = "qwertyuiop"
+        self.client.login(username=self.admin.email, password="azerty")
+        response = self.client.post(
+            self.form_url,
+            {"old_password": "azerty", "new_password1": password, "new_password2": password, "form-mod": "password"},
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Les modifications ont bien été prises en compte!")
+
+        self.assertTrue(MaintenanceUser.objects.get(pk=self.admin.pk).check_password(password))
