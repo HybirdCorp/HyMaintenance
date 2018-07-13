8b9067f7a989b00f296c1c5eca7a6e3d8c2f2913
Niouby <morgane.alonso@protonmail.com>
2018-07-13 17:33:59 +0200

    Refs 117, add test on create credit form and view

---
+++ hymaintenance/high_ui/tests/views/test_credit_view.py
@@ -0,0 +1,79 @@
+from django.test import RequestFactory
+from django.test import TestCase
+from django.urls import reverse
+
+from customers.tests.factories import AdminUserFactory
+from customers.tests.factories import ManagerUserFactory
+from customers.tests.factories import OperatorUserFactory
+from maintenance.models import MaintenanceCredit
+from maintenance.models.contract import AVAILABLE_TOTAL_TIME
+from maintenance.models.contract import CONSUMMED_TOTAL_TIME
+from maintenance.tests.factories import create_project
+
+from ...views.credit import CreditCreateView
+
+
+class CreditCreateViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = AdminUserFactory(email="gordon.freeman@blackmesa.com", password="azerty")
+        cls.company, cls.c1, cls.c2, cls.c3 = create_project(
+            contract1={"total_type": AVAILABLE_TOTAL_TIME},
+            contract2={"total_type": AVAILABLE_TOTAL_TIME},
+            contract3={"total_type": CONSUMMED_TOTAL_TIME},
+        )
+        cls.form_url = reverse("high_ui:project-create_credit", kwargs={"company_name": cls.company.slug_name})
+        cls.login_url = reverse("login") + "?next=" + cls.form_url
+
+    def test_get_context_data(self):
+        factory = RequestFactory()
+        request = factory.get(self.form_url)
+        request.user = self.user
+        view = CreditCreateView()
+        view.request = request
+        view.company = self.company
+        view.object = self.company
+        view.hours_step = 8
+        view.user = self.user
+
+        context = view.get_context_data()
+        self.assertEqual([8, 16, 24, 32, 40], list(context["hours_numbers"]))
+        self.assertIn(self.c1, context["available_time_contracts"])
+        self.assertIn(self.c2, context["available_time_contracts"])
+        self.assertNotIn(self.c3, context["available_time_contracts"])
+
+    def test_manager_cannot_get_create_form(self):
+        ManagerUserFactory(email="chell@aperture-science.com", password="azerty")
+
+        self.client.login(username="chell@aperture-science.com", password="azerty")
+        response = self.client.get(self.form_url)
+
+        self.assertRedirects(response, self.login_url)
+
+    def test_operator_can_get_create_form(self):
+        operator = OperatorUserFactory(email="chell@aperture-science.com", password="azerty")
+        operator.operator_for.add(self.company)
+
+        self.client.login(username="chell@aperture-science.com", password="azerty")
+        response = self.client.get(self.form_url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_can_get_create_form(self):
+        self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
+        response = self.client.get(self.form_url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_can_post_form_to_create_a_project(self):
+        self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
+
+        response = self.client.post(self.form_url, {"hours_number": 16, "maintenance_type": 1}, follow=True)
+
+        self.assertRedirects(
+            response, reverse("high_ui:project_details", kwargs={"company_name": self.company.slug_name})
+        )
+
+        self.assertEqual(
+            16, MaintenanceCredit.objects.filter(maintenance_type=1, company=self.company).first().hours_number
+        )
--- hymaintenance/maintenance/tests/forms/test_credit_forms.py
+++ hymaintenance/maintenance/tests/forms/test_credit_forms.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.translation import ugettext_lazy as _
 
 from customers.tests.factories import CompanyFactory
 
@@ -9,6 +10,16 @@ from ...models import MaintenanceCredit
 class MaintenanceCreditModelFormTestCase(TestCase):
     def setUp(self):
         self.company = CompanyFactory()
+
+    def test_create_form_maintenance_type_initial_values(self):
+        form = MaintenanceCreditCreateForm(company=self.company, hours_number_initial=8)
+        self.assertEqual(8, form.get_initial_for_field(form.fields["hours_number"], "hours_number"))
+
+    def test_all_required_fields_by_sending_a_empty_create_form(self):
+        form = MaintenanceCreditCreateForm(company=self.company, hours_number_initial=8, data={})
+        self.assertFalse(form.is_valid())
+        expected = _("This field is required.")
+        self.assertDictEqual(form.errors, {"maintenance_type": [expected], "hours_number": [expected]})
 
     def test_create_credit_with_model_form(self):
         hours_number = 10
