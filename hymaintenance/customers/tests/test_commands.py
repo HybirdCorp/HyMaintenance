aa02079252e6e7ad30d74d3a095ab66da44286de
Niouby <morgane.alonso@protonmail.com>
2018-08-01 15:14:00 +0200

    Refs 222, add tests on create_admin command

---
+++ hymaintenance/customers/tests/test_commands.py
@@ -0,0 +1,45 @@
+from io import StringIO
+
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils.translation import gettext as _
+
+from high_ui.tests.utils import SetDjangoLanguage
+
+from ..models.user import MaintenanceUser
+from .factories import AdminUserFactory
+
+
+class CreateAdminCommandTestCase(TestCase):
+    def test_create_admin_when_no_admin_already_exists(self):
+        with SetDjangoLanguage("en"):
+            output = StringIO()
+
+            call_command("create_admin", stdout=output)
+
+            self.assertEqual(1, MaintenanceUser.objects.filter(is_superuser=True).count())
+            self.assertTrue(MaintenanceUser.objects.filter(email="admin@mail.com"))
+            self.assertIn(_("Successfully create an admin user."), output.getvalue())
+
+    def test_create_admin_when_one_admin_already_exists(self):
+        with SetDjangoLanguage("en"):
+            output = StringIO()
+            AdminUserFactory()
+
+            call_command("create_admin", stdout=output)
+
+            self.assertEqual(1, MaintenanceUser.objects.filter(is_superuser=True).count())
+            self.assertFalse(MaintenanceUser.objects.filter(email="admin@mail.com"))
+            self.assertIn(_("There is already one admin user."), output.getvalue())
+
+    def test_create_admin_when_two_admins_already_exist(self):
+        with SetDjangoLanguage("en"):
+            output = StringIO()
+            AdminUserFactory()
+            AdminUserFactory()
+
+            call_command("create_admin", stdout=output)
+
+            self.assertEqual(2, MaintenanceUser.objects.filter(is_superuser=True).count())
+            self.assertFalse(MaintenanceUser.objects.filter(email="admin@mail.com"))
+            self.assertIn(_("There are already {} admin users.").format(2), output.getvalue())
