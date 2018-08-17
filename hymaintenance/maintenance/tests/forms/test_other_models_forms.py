e037f12f6bb92381b4f283db26a08817ca83b61d
Niouby <morgane.alonso@protonmail.com>
2018-08-17 16:47:03 +0200

    Refs 249, add tests on maintenance_type update form

--- hymaintenance/maintenance/tests/forms/test_other_models_forms.py
+++ hymaintenance/maintenance/tests/forms/test_other_models_forms.py
@@ -11,12 +11,21 @@ class MaintenanceTypeUpdateFormTestCase(TestCase):
     def setUpTestData(cls):
         cls.user = AdminUserFactory(email="gordon.freeman@blackmesa.com", password="azerty")
 
-    def __get_dict_for_post(self):
-        return {
-            "maintenance_type1_name": "The cake",
-            "maintenance_type2_name": "is",
-            "maintenance_type3_name": "a lie.",
-        }
+    def test_form_with_initial_args(self):
+        form = MaintenanceTypeUpdateForm(initial={"maintenance_type1_name": "lala"})
+        maintenance_types = MaintenanceType.objects.order_by("id")
+        self.assertEqual(
+            maintenance_types[0].name,
+            form.get_initial_for_field(form.fields["maintenance_type1_name"], "maintenance_type1_name"),
+        )
+        self.assertEqual(
+            maintenance_types[1].name,
+            form.get_initial_for_field(form.fields["maintenance_type2_name"], "maintenance_type2_name"),
+        )
+        self.assertEqual(
+            maintenance_types[2].name,
+            form.get_initial_for_field(form.fields["maintenance_type3_name"], "maintenance_type3_name"),
+        )
 
     def test_maintenance_type_update_form_initial_values(self):
         form = MaintenanceTypeUpdateForm()
@@ -47,8 +56,12 @@ class MaintenanceTypeUpdateFormTestCase(TestCase):
             },
         )
 
-    def test_valid_form_update_maintenance_type_name(self):
-        dict_for_post = self.__get_dict_for_post()
+    def test_valid_form_to_update_maintenance_type_with_different_names(self):
+        dict_for_post = {
+            "maintenance_type1_name": "The cake",
+            "maintenance_type2_name": "is",
+            "maintenance_type3_name": "a lie.",
+        }
         form = MaintenanceTypeUpdateForm(data=dict_for_post)
 
         is_valid = form.is_valid()
@@ -58,3 +71,20 @@ class MaintenanceTypeUpdateFormTestCase(TestCase):
         self.assertEqual("The cake", MaintenanceType.objects.all().order_by("id")[0].name)
         self.assertEqual("is", MaintenanceType.objects.order_by("id")[1].name)
         self.assertEqual("a lie.", MaintenanceType.objects.order_by("id")[2].name)
+
+    def test_valid_form_to_update_maintenance_type_with_same_names(self):
+        maintenance_types = MaintenanceType.objects.all().order_by("id")
+        dict_for_post = {
+            "maintenance_type1_name": maintenance_types[0].name,
+            "maintenance_type2_name": maintenance_types[1].name,
+            "maintenance_type3_name": maintenance_types[2].name,
+        }
+        form = MaintenanceTypeUpdateForm(data=dict_for_post)
+
+        is_valid = form.is_valid()
+        self.assertTrue(is_valid)
+
+        form.update_maintenance_types_names()
+        self.assertEqual(maintenance_types[0].name, MaintenanceType.objects.all().order_by("id")[0].name)
+        self.assertEqual(maintenance_types[1].name, MaintenanceType.objects.order_by("id")[1].name)
+        self.assertEqual(maintenance_types[2].name, MaintenanceType.objects.order_by("id")[2].name)
