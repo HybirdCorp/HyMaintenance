77b06b4a0c77de6d2173c4ba146e604d5c21176a
Niouby <morgane.alonso@protonmail.com>
2018-09-03 16:50:39 +0200

    Refs 50, add general information update form

---
+++ hymaintenance/high_ui/views/general_information.py
@@ -0,0 +1,27 @@
+
+from django.urls import reverse
+from django.views.generic import UpdateView
+
+from ..forms import GeneralInformationModelForm
+from ..models import GeneralInformation
+from .base import IsAdminTestMixin
+from .base import get_context_data_dashboard_header
+from .base import get_context_data_footer
+
+
+class GeneralInformationUpdateView(IsAdminTestMixin, UpdateView):
+    form_class = GeneralInformationModelForm
+    template_name = "high_ui/forms/update_infos.html"
+    model = GeneralInformation
+
+    def get_success_url(self):
+        return reverse("high_ui:admin")
+
+    def get_object(self):
+        return GeneralInformation.objects.all().first()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_context_data_dashboard_header(self.user))
+        context.update(get_context_data_footer())
+        return context
