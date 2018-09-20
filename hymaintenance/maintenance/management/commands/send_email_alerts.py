0dc39feaaf2d9b1a55ef288c71cd0303396f0168
Niouby <morgane.alonso@protonmail.com>
2018-09-20 16:39:53 +0200

    Refs 266, add send_email_alert command
    pour lancer tous les lundi la commande avec crontab
        $ crontab -e
        * * * * 1 python manage.py send_email_alerts

---
+++ hymaintenance/maintenance/management/commands/send_email_alerts.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.utils.translation import gettext as _
+
+from maintenance.models.contract import MaintenanceContract
+from toolkit.email import is_number_hours_min_exceeded
+from toolkit.email import send_email_alert
+
+
+class Command(BaseCommand):
+    help = _(
+        """This command do for each contracts :
+           * check if remaining minutes reach the minutes minimum
+           * if yes, send an alert email to the client"""
+    )
+
+    def handle(self, *args, **options):
+        contracts = MaintenanceContract.objects.all()
+        for contract in contracts:
+            if contract.recipient and contract.number_hours_min and is_number_hours_min_exceeded(contract):
+                send_email_alert(contract)
