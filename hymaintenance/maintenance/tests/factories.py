7d38d20b1891133ac28c57282a974c28dad82d9d
MrJmad <j-mad@j-mad.com>
2018-10-03 23:39:46 +0200

    remove redundant parentheses

--- hymaintenance/maintenance/tests/factories.py
+++ hymaintenance/maintenance/tests/factories.py
@@ -37,7 +37,7 @@ def create_project(**kwargs):
         )
     else:
         contract3 = MaintenanceContractFactory(company=company, maintenance_type=MaintenanceType.objects.get(id=3))
-    return (company, contract1, contract2, contract3)
+    return company, contract1, contract2, contract3
 
 
 class IncomingChannelFactory(factory.django.DjangoModelFactory):
