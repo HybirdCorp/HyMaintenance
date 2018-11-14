82e1a9a1c13352d9b1e7dfd240561922daffbef6
Niouby <morgane.alonso@protonmail.com>
2018-11-14 15:58:30 +0100

    Refs 315, fix and add tests for update credits

--- hymaintenance/maintenance/tests/forms/test_credit_forms.py
+++ hymaintenance/maintenance/tests/forms/test_credit_forms.py
@@ -2,11 +2,13 @@ from django.test import TestCase
 from django.utils.translation import ugettext_lazy as _
 
 from ...forms.credit import MaintenanceCreditCreateForm
+from ...forms.credit import MaintenanceCreditUpdateForm
 from ...models import MaintenanceCredit
+from ..factories import MaintenanceCreditFactory
 from ..factories import create_project
 
 
-class MaintenanceCreditModelFormTestCase(TestCase):
+class MaintenanceCreditCreateFormTestCase(TestCase):
     def setUp(self):
         self.company, self.contract, _, _ = create_project()
 
@@ -20,7 +22,7 @@ class MaintenanceCreditModelFormTestCase(TestCase):
         expected = _("This field is required.")
         self.assertDictEqual(form.errors, {"contract": [expected], "hours_number": [expected]})
 
-    def test_create_credit_with_model_form(self):
+    def test_create_credit_with_create_form(self):
         hours_number = 10
         form = MaintenanceCreditCreateForm(
             company=self.company,
@@ -30,3 +32,16 @@ class MaintenanceCreditModelFormTestCase(TestCase):
         self.assertTrue(form.is_valid(), form.errors)
         form.save()
         self.assertEqual(1, MaintenanceCredit.objects.filter(company=self.company, contract=self.contract).count())
+
+
+class MaintenanceCreditUpdateFormTestCase(TestCase):
+    def test_update_credit_with_update_form(self):
+        company, c1, c2, _ = create_project()
+        credit = MaintenanceCreditFactory(company=company, contract=c1, hours_number=8)
+        hours_number = 10
+        form = MaintenanceCreditUpdateForm(instance=credit, data={"hours_number": hours_number, "contract": c2.pk})
+        self.assertTrue(form.is_valid(), form.errors)
+        form.save()
+        updated_credit = MaintenanceCredit.objects.get(pk=credit.pk)
+        self.assertEqual(hours_number, updated_credit.hours_number)
+        self.assertEqual(c2, updated_credit.contract)
