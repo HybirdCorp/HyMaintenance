b514c404bc37b2c6ebd900b4f572d7a1a2865d3d
Niouby <morgane.alonso@protonmail.com>
2018-11-14 15:58:30 +0100

    Refs 315, fix and add tests for update credits

--- hymaintenance/high_ui/tests/views/test_credit_view.py
+++ hymaintenance/high_ui/tests/views/test_credit_view.py
@@ -8,9 +8,11 @@ from customers.tests.factories import OperatorUserFactory
 from maintenance.models import MaintenanceCredit
 from maintenance.models.contract import AVAILABLE_TOTAL_TIME
 from maintenance.models.contract import CONSUMMED_TOTAL_TIME
+from maintenance.tests.factories import MaintenanceCreditFactory
 from maintenance.tests.factories import create_project
 
 from ...views.credit import CreditCreateView
+from ...views.credit import CreditUpdateView
 
 
 class CreditCreateViewTestCase(TestCase):
@@ -79,17 +81,127 @@ class CreditCreateViewTestCase(TestCase):
         )
 
     def test_create_form_has_good_contract_ids(self):
-        company, c1, c2, _ = create_project(
-            contract1={"total_type": AVAILABLE_TOTAL_TIME}, contract2={"total_type": AVAILABLE_TOTAL_TIME}
+        company, c1, c2, c3 = create_project(
+            company={"name": "Aperture Science"},
+            contract1={"total_type": AVAILABLE_TOTAL_TIME},
+            contract2={"total_type": AVAILABLE_TOTAL_TIME},
+            contract3={"total_type": CONSUMMED_TOTAL_TIME},
         )
-        form_url = reverse("high_ui:project-create_credit", kwargs={"company_name": company.slug_name})
+        credit = MaintenanceCreditFactory(company=company, contract=c1)
+        form_url = reverse("high_ui:project-update_credit", kwargs={"company_name": company.slug_name, "pk": credit.pk})
 
+        factory = RequestFactory()
+        request = factory.get(form_url)
+        request.user = self.user
+        view = CreditUpdateView()
+        view.request = request
+        view.company = company
+        view.object = company
+        view.hours_step = 8
+        view.user = self.user
+
+        context = view.get_context_data()
+        self.assertIn(c1, context["available_time_contracts"])
+        self.assertIn(c2, context["available_time_contracts"])
+        self.assertNotIn(c3, context["available_time_contracts"])
+        self.assertNotIn(self.c1, context["available_time_contracts"])
+        self.assertNotIn(self.c2, context["available_time_contracts"])
+        self.assertNotIn(self.c3, context["available_time_contracts"])
+
+
+class CreditUpdateViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = AdminUserFactory(email="gordon.freeman@blackmesa.com", password="azerty")
+        cls.company, cls.c1, cls.c2, cls.c3 = create_project(
+            contract1={"total_type": AVAILABLE_TOTAL_TIME},
+            contract2={"total_type": AVAILABLE_TOTAL_TIME},
+            contract3={"total_type": CONSUMMED_TOTAL_TIME},
+        )
+
+    def setUp(self):
+        self.credit = MaintenanceCreditFactory(company=self.company, contract=self.c1, hours_number=8)
+        self.form_url = reverse(
+            "high_ui:project-update_credit", kwargs={"company_name": self.company.slug_name, "pk": self.credit.id}
+        )
+        self.login_url = reverse("login") + "?next=" + self.form_url
+
+    def test_get_context_data(self):
+        factory = RequestFactory()
+        request = factory.get(self.form_url)
+        request.user = self.user
+        view = CreditUpdateView()
+        view.request = request
+        view.company = self.company
+        view.object = self.company
+        view.hours_step = 8
+        view.user = self.user
+
+        context = view.get_context_data()
+        self.assertEqual([8, 16, 24, 32, 40], list(context["hours_numbers"]))
+        self.assertIn(self.c1, context["available_time_contracts"])
+        self.assertIn(self.c2, context["available_time_contracts"])
+        self.assertNotIn(self.c3, context["available_time_contracts"])
+
+    def test_manager_cannot_get_update_form(self):
+        ManagerUserFactory(email="chell@aperture-science.com", password="azerty")
+
+        self.client.login(username="chell@aperture-science.com", password="azerty")
+        response = self.client.get(self.form_url)
+
+        self.assertRedirects(response, self.login_url)
+
+    def test_operator_can_get_update_form(self):
+        operator = OperatorUserFactory(email="chell@aperture-science.com", password="azerty")
+        operator.operator_for.add(self.company)
+
+        self.client.login(username="chell@aperture-science.com", password="azerty")
+        response = self.client.get(self.form_url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_can_get_update_form(self):
+        self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
+        response = self.client.get(self.form_url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_can_post_form_to_update_credit(self):
         self.client.login(username="gordon.freeman@blackmesa.com", password="azerty")
 
-        response = self.client.get(form_url)
+        response = self.client.post(self.form_url, {"hours_number": 16, "contract": self.c2.pk}, follow=True)
 
-        self.assertContains(response, 'data-selector-value="{cid}"'.format(cid=c1.id))
-        self.assertContains(response, 'data-selector-value="{cid}"'.format(cid=c2.id))
-        self.assertNotContains(response, 'data-selector-value="{cid}"'.format(cid=self.c1.id))
-        self.assertNotContains(response, 'data-selector-value="{cid}"'.format(cid=self.c2.id))
-        self.assertNotContains(response, 'data-selector-value="{cid}"'.format(cid=self.c3.id))
+        self.assertRedirects(
+            response, reverse("high_ui:project_details", kwargs={"company_name": self.company.slug_name})
+        )
+        credit = MaintenanceCredit.objects.get(pk=self.credit.pk)
+        self.assertEqual(self.c2, credit.contract)
+        self.assertEqual(16, credit.hours_number)
+
+    def test_update_form_has_good_contract_ids(self):
+        company, c1, c2, c3 = create_project(
+            company={"name": "Aperture Science"},
+            contract1={"total_type": AVAILABLE_TOTAL_TIME},
+            contract2={"total_type": AVAILABLE_TOTAL_TIME},
+            contract3={"total_type": CONSUMMED_TOTAL_TIME},
+        )
+        credit = MaintenanceCreditFactory(company=company, contract=c1)
+        form_url = reverse("high_ui:project-update_credit", kwargs={"company_name": company.slug_name, "pk": credit.pk})
+
+        factory = RequestFactory()
+        request = factory.get(form_url)
+        request.user = self.user
+        view = CreditUpdateView()
+        view.request = request
+        view.company = company
+        view.object = company
+        view.hours_step = 8
+        view.user = self.user
+
+        context = view.get_context_data()
+        self.assertIn(c1, context["available_time_contracts"])
+        self.assertIn(c2, context["available_time_contracts"])
+        self.assertNotIn(c3, context["available_time_contracts"])
+        self.assertNotIn(self.c1, context["available_time_contracts"])
+        self.assertNotIn(self.c2, context["available_time_contracts"])
+        self.assertNotIn(self.c3, context["available_time_contracts"])
