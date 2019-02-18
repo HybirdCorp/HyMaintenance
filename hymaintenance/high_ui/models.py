0fd6f08113320053871626e4e2abc702e638483f
Niouby <morgane.alonso@protonmail.com>
2019-02-18 11:55:38 +0100

    Refs 283, modify url to charfield for website in generalinfo

--- hymaintenance/high_ui/models.py
+++ hymaintenance/high_ui/models.py
@@ -8,5 +8,5 @@ class GeneralInformation(models.Model):
     name = models.CharField(_("Name"), max_length=255)
     email = LowerCaseEmailField(_("Email address"), max_length=255)
     address = models.TextField(_("postal address"))
-    website = models.URLField(_("Website"), max_length=255)
+    website = models.CharField(_("Website"), max_length=255)
     phone = models.CharField(_("phone number"), max_length=25, blank=True, null=True)
