66a28ffc77845feef227a4906a045c9d82d95736
Niouby <morgane.alonso@protonmail.com>
2019-04-23 16:40:26 +0200

    Refs 429, add date in create an update credit forms

--- hymaintenance/maintenance/forms/credit.py
+++ hymaintenance/maintenance/forms/credit.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.timezone import now
 
 from ..models import MaintenanceContract
 from ..models import MaintenanceCredit
@@ -8,13 +9,14 @@ from ..models.credit import MaintenanceCreditChoices
 class MaintenanceCreditCreateForm(forms.ModelForm):
     class Meta:
         model = MaintenanceCredit
-        fields = ("hours_number", "contract", "subject")
+        fields = ("hours_number", "contract", "subject", "date")
         widgets = {"hours_number": forms.TextInput(), "contract": forms.HiddenInput()}
 
     def __init__(self, *args, **kwargs):
         self.company = kwargs.pop("company")
         super().__init__(*args, **kwargs)
         self.fields["hours_number"].initial = MaintenanceCreditChoices.objects.all().order_by("id").first().value
+        self.fields["date"].initial = now().date()
         self.fields["contract"].queryset = MaintenanceContract.objects.filter(company=self.company)
 
     def save(self, commit=True):
@@ -25,5 +27,5 @@ class MaintenanceCreditCreateForm(forms.ModelForm):
 class MaintenanceCreditUpdateForm(forms.ModelForm):
     class Meta:
         model = MaintenanceCredit
-        fields = ("hours_number", "contract", "subject")
+        fields = ("hours_number", "contract", "subject", "date")
         widgets = {"hours_number": forms.TextInput(), "contract": forms.HiddenInput()}
