75b4bdbb4425c73b523252e2c30dfceadb2062bd
Morgane Alonso <malonso@hybird.org>
2022-04-21 17:09:37 +0200

    update lint requirements in tox and refactor requirements files

--- tasks/requirements.py
+++ tasks/requirements.py
@@ -2,7 +2,9 @@ from invoke import task
 
 
 @task
-def install_req(c, lints=True):
+def install_req(c, lints=True, tests=True):
     c.run("pip install -r requirements.txt")
     if lints:
         c.run("pip install -r requirements_lints.txt")
+    if tests:
+        c.run("pip install -r requirements_tests.txt")
